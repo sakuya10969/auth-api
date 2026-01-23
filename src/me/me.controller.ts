05dc3823a0fe4660f8b20d025ca0e5c4c25da6dc
sakuya10969 <sakuya.f20010618@gmail.com>
2026-01-23 15:04:49 +0900

    feat: authorization

---
+++ src/me/me.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import type { Request } from 'express';
+
+import { JwtGuard } from 'src/auth/jwt.guard';
+
+@Controller()
+export class MeController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  me(@Req() req: Request & { user: { userId: string; email: string } }) {
+    return req.user;
+  }
+}
