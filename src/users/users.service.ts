173930f67de020a4c18c9bf4781aa9acaa68f46a
sakuya10969 <sakuya.f20010618@gmail.com>
2026-01-23 15:04:21 +0900

    feat: users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  findById(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  createUser(email: string, passwordHash: string) {
+    return this.prisma.user.create({ data: { email, passwordHash } });
+  }
+
+  setRefreshTokenHash(userId: string, refreshTokenHash: string | null) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { refreshTokenHash },
+    });
+  }
+}
