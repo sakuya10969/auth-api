edb4e64c9eb2545fa1b6ac786a7b9616e9bfb525
sakuya10969 <sakuya.f20010618@gmail.com>
2026-01-29 09:58:37 +0900

    feat: me

--- src/me/me.controller.ts
+++ src/me/me.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import type { Request } from 'express';
 
-import { JwtGuard } from 'src/auth/jwt.guard';
+import { JwtGuard } from '@/auth/jwt.guard';
 
 @Controller()
 export class MeController {
   @UseGuards(JwtGuard)
   @Get('me')
-  me(@Req() req: Request & { user: { userId: string; email: string } }) {
+  me(@Req() req: Request & { user: { userId: string } }) {
     return req.user;
   }
 }
